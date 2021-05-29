940e9000aa580b7530fb0147cd6234a63e907793
Artemiy Matchin <art16ws@gmail.com>
2021-05-29 13:19:13 +0300

    ver 1

--- Member.py
+++ Member.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 class Member:
     id = 0
     first_name = ''
@@ -7,13 +10,48 @@ class Member:
     city = ''
     university = ''
     graduation = ''
+    company = ''
+    position = ''
 
     def __init__(self, raw_member):
         self.id = raw_member['id']
         self.first_name = raw_member['first_name']
         self.last_name = raw_member['last_name']
-        self.country = raw_member['country']['title']
-        self.city = raw_member['city']['title']
-        self.university = raw_member['university']['title']
 
-        date_str = raw_member['bdate']
+        if 'country' in raw_member:
+            self.country = raw_member['country']['title']
+
+        if 'city' in raw_member:
+            self.city = raw_member['city']['title']
+
+        if 'university_name' in raw_member:
+            print(raw_member['university_name'] + ' --- ' + raw_member['faculty_name'])
+            self.university = raw_member['university_name']
+            self.graduation = raw_member['graduation']
+
+        if 'career' in raw_member:
+            career_list = list(raw_member['career'])
+            if len(career_list) != 0:
+                if isinstance(career_list, list):
+                    career_list = list(career_list)
+                if 'company' in career_list[-1]:
+                    self.company = career_list[-1]['company']
+                if 'position' in career_list[-1]:
+                    self.position = career_list[-1]['position']
+                if 'group_id' in career_list[-1]:
+                    self.company = career_list[-1]['group_id']
+
+        if 'bdate' in raw_member and len(raw_member['bdate']) > 6:
+            bdate = datetime.strptime(raw_member['bdate'], '%d.%m.%Y')
+            cur_date = datetime.today()
+            self.age = cur_date - bdate
+
+    def get_param(self, param_name):
+        if param_name == 'city':
+            return self.city
+        if param_name == 'university':
+            return self.university
+        if param_name == 'company':
+            return self.company
+        if param_name == 'position':
+            return self.position
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import requests as r
 import json
+from Member import Member
+from collections import OrderedDict
 
 
 # Only for 1000 members!
 def get_members():
-
     request_params = {'v': 5.131,
                       'group_id': 'rosatomcareer',
                       'sort': 'id_asc',
@@ -15,18 +16,70 @@ def get_members():
 
     res = r.get('https://api.vk.com/method/groups.getMembers', params=request_params)
     members_ids = res.json()['response']['items']
-
     return members_ids
 
 
+def get_group_names(groups):
+    groups_str = ''
+    for g in groups:
+        groups_str += str(g)
+        groups_str += ','
+
+    groups_str = groups_str[:-1]
+
+    request_params = {'v': 5.131,
+                      'group_ids': groups_str,
+                      'access_token': 'd9acf98cd9acf98cd9acf98c96d9de6273dd9acd9acf98c874aa57b9b6642522b5a44e4'
+                      }
+    res = r.get('https://api.vk.com/method/groups.getById', params=request_params)
+    res = res.json()
+    groups_dict = {}
+    for elem in res['response']:
+        groups_dict[str(elem['id'])] = elem['name']
+    return groups_dict
+
+
+def specify_companies(members):
+    comp_to_specify = []
+    for m in members:
+        company_id = m.get_param('company')
+        if isinstance(company_id, int):
+            comp_to_specify.append(company_id)
+
+    company_names = get_group_names(comp_to_specify)
+    for m in members:
+        company_id = m.get_param('company')
+        if isinstance(company_id, int):
+            m.company = company_names[str(company_id)]
+
+
 def convert_to_member_class(member_base_info):
-    members = []
+    new_members = []
+    for m in member_base_info['response']:
+
+        if 'is_closed' in m and m['is_closed'] is False \
+                and 'bdate' in m and len(m['bdate']) > 6:
+            new_member = Member(m)
+            new_members.append(new_member)
+    return new_members
+
+
+def make_top(members, param):
+    cnt_dict = {}
+    for m in members:
+        key = m.get_param(param)
+        if key not in cnt_dict:
+            cnt_dict[key] = 1
+        else:
+            cnt_dict[key] += 1
+    top_dict = dict(sorted(cnt_dict.items(), key=lambda item: item[1], reverse=True))
+    top_dict.pop('')
+    return top_dict
 
 
 def get_member_base_info(members_ids):
-
     ids_json = json.dumps(members_ids)
-    extra_fields = 'country,city,bdate,education'
+    extra_fields = 'country,city,bdate,education,career'
     request_params = {'v': 5.131,
                       'user_ids': ids_json,
                       'access_token': 'd9acf98cd9acf98cd9acf98c96d9de6273dd9acd9acf98c874aa57b9b6642522b5a44e4',
@@ -35,11 +88,21 @@ def get_member_base_info(members_ids):
     res = r.get('https://api.vk.com/method/users.get', params=request_params)
     member_base_info = res.json()
 
-
-
     return member_base_info
 
 
+def execute():
+    r_members = get_members()
+    members_bi = get_member_base_info(r_members)
+    r_members = convert_to_member_class(members_bi)
+    specify_companies(r_members)
+    city_stats = make_top(r_members, 'city')
+    edu_stats = make_top(r_members, 'university')
+    career_stats = make_top(r_members, 'company')
+    pos_stats = make_top(r_members, 'position')
+
+    print(0)
+
+
 if __name__ == '__main__':
-    members = get_members()
-    get_member_base_info(members)
+    execute()
