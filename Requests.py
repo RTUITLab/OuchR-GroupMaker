7152d480b38c6f83de56f2ffb183311e251c86dd
Artemiy Matchin <art16ws@gmail.com>
2021-05-30 13:14:59 +0300

    Some refactoring

--- Requests.py
+++ Requests.py
@@ -28,40 +28,11 @@ class Requests:
         print(0)
 
     @staticmethod
-    def get_members(params, total):
-        result = []
-        params['count'] = 1000
-        loaded = 0
-        while loaded < total:
-            response = r.get('https://api.vk.com/method/groups.getMembers', params=params).json()
-            # Uncomment for more then 1000 users
-            # total = response['response']['count']
-            response = response['response']['items']
-            result.append(response)
-            loaded += 1000
-            params['offset'] = loaded
-        return result
-
-    @staticmethod
     def load_token():
         qtoken_file = open("tkn_q", 'r')
         token_file = open("tkn", 'r')
         Requests.qtoken = qtoken_file.read()
         Requests.token = token_file.read()
-
-    @staticmethod
-    def get_members_proto():
-        raw_script = Requests.load_script('getMembersScript')
-        param_script = raw_script.format(access_token=Requests.qtoken,
-                                         group_id=Requests.group_id)
-
-        params = {
-            'v': '5.131',
-            'access_token': Requests.token,
-            'code': param_script
-            }
-        result = r.get('https://api.vk.com/method/execute', params=params).json()['response']
-        return result
 
     @staticmethod
     def get_member_info_proto():
